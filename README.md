c5e0b9ddba796660c3abbf1c765999ef8db3bcf9
sansregret <695035427@qq.com>
2018-02-24 23:28:24 +0800

    1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# my-fend-project-three-feed-reader-testing
+Udacity前端进阶纳米学位，项目3，订阅阅读器测试
+=======
+# 街机游戏克隆项目-Frogger
+
+## <i class="icon-list"></i> 本页索引
+
+* [点击查看网页](#点击查看网页)
+* [项目说明](#项目说明)
+* [测试说明](#测试说明)
+
+## 点击查看网页
+
+[订阅阅读器测试（点击查看）](https://sansregret.github.io/udacity/project-three/index)
+
+## 项目说明
+
+测试工具：jasmine。
+
+四个测试用例，共计七个测试。
+
+## 测试说明
+
+- RSS Feeds 测试用例
+    - 第一个测试 'are defined' - 用来保证 'allFeeds' 变量被定义了而且不是空的。
+      测试：'app.js' 里面的 'allFeeds' 变量已定义，'allFeeds' 的长度不为0。
+    - 第二个测试 'have url and are not null' - 遍历 'allFeeds' 对象里面的所有的源来保证有链接字段而且链接不是空的。
+      测试：遍历 'allFeeds' 的 'url' 均不为空。
+    - 第三个测试 'have name and are not null' - 遍历 'allFeeds' 对象里面的所有的源来保证有名字字段而且不是空的。
+      测试：遍历 'allFeeds' 的 'name' 均不为空。
+
+- The menu 测试用例
+    - 第一个测试 'item is hidden' - 用来保证保证菜单元素默认是隐藏的。
+      测试：分析css文件中存在 '.menu-hidden' 类， '<body>' 标签有了这个类便显示菜单元素，所以只需要测试一开始带有这个类的元素存在。
+    - 第二个测试 'can switch' - 用来保证当菜单图标被点击的时候菜单会切换可见状态。
+      测试：分析测试先点击一次带有 '.menu-icon-link' 类的“按钮”，'<body>' 标签不带有 '.menu-hidden' 类；再点击一次带有 '.menu-icon-link' 类的“按钮”，'<body>' 标签不、带有 '.menu-hidden' 类。
+
+- Initial Entries 测试用例
+    - 第一个测试 'loadFeed works' - 用来保证 'loadFeed' 函数被调用而且工作正常。
+      测试：在 'beforeEach' 中结合 'done()' 异步实现 'loadFeed(0)' 事件，测试在 '.feed' 容器元素里面至少有一个 '.entry' 的元素。
+
+- New Feed Selection 测试用例
+    - 第一个测试 'loadFeed will change' - 用来保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
+      测试：先用一个变量 'EnIn0' 装载加载新源前的第一个 '.entry' 中的 'innerText' ，在 'beforeEach' 中结合 'done()' 异步实现 'loadFeed(1)' 事件，再用一个变量 'EnIn1' 装载加载新源后的第一个 '.entry' 中的 'innerText' ，最后测试'EnIn1'与'EnIn0'是否不等。
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,107 +3,101 @@
  * 这是 Jasmine 会读取的spec文件，它包含所有的要在你应用上面运行的测试。
  */
 
-/* 我们把所有的测试都放在了 $() 函数里面。因为有些测试需要 DOM 元素。
- * 我们得保证在 DOM 准备好之前他们不会被运行。
- */
+/* 我们把所有的测试都放在了 $() 函数里面。因为有些测试需要 DOM 元素。 我们得保证在 DOM 准备好之前他们不会被运行。 */
 $(function() {
-    /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
+  /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
      * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
     */
-    describe('RSS Feeds', function() {
-        /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
-         * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
-         * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
-         * 页面看看会发生什么。
+  describe('RSS Feeds', function() {
+    /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
+         * 不是空的。
         */
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
-
-
-        /*
-         * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
-         */
-        it('have url and are not null', function(){
-          for(let i=0;i<allFeeds.length;i++){
-            expect(allFeeds[i].url).not.toBeNull();
-          }
-        });
-
-        /*
-         * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
-         */
-         it('have name and are not null', function(){
-           for(let i=0;i<allFeeds.length;i++){
-             expect(allFeeds[i].name).not.toBeNull();
-           }
-         });
+    it('are defined', function() {
+      expect(allFeeds).toBeDefined();
+      expect(allFeeds.length).not.toBe(0);
     });
 
+    /*
+         * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
+         */
+    it('have url and are not null', function() {
+      for (let i = 0; i < allFeeds.length; i++) {
+        expect(allFeeds[i].url).not.toBeNull();
+      }
+    });
 
-    /* 写一个叫做 "The menu" 的测试用例 */
-    describe("The menu",function(){
-        let MH=document.getElementsByClassName("menu-hidden");
-        let MIL=document.getElementsByClassName("menu-icon-link");
-        /*
+    /*
+         * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
+         */
+    it('have name and are not null', function() {
+      for (let i = 0; i < allFeeds.length; i++) {
+        expect(allFeeds[i].name).not.toBeNull();
+      }
+    });
+  });
+
+  /* 写一个叫做 "The menu" 的测试用例 */
+  describe("The menu", function() {
+    let MH = document.getElementsByClassName("menu-hidden");
+    let MIL = document.getElementsByClassName("menu-icon-link");
+    /*
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
-         it("item is hidden",function(){
-           expect(MH[0]).toBeDefined();
-         });
+    it("item is hidden", function() {
+      expect(MH[0]).toBeDefined();
+    });
 
-         /*
+    /*
           * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
           * 测试应该包含两个 expectation ： 当点击图标的时候菜单是否显示，
           * 再次点击的时候是否隐藏。
           */
-          it("can switch",function(){
-            // 当点击图标的时候菜单是否显示
-            MIL[0].click();
-            expect(MH[0]).not.toBeDefined();
-            // 再次点击的时候是否隐藏
-            MIL[0].click();
-            expect(MH[0]).toBeDefined();
-          });
+    it("can switch", function() {
+      // 当点击图标的时候菜单是否显示
+      MIL[0].click();
+      expect(MH[0]).not.toBeDefined();
+      // 再次点击的时候是否隐藏
+      MIL[0].click();
+      expect(MH[0]).toBeDefined();
     });
+  });
 
-    /* 写一个叫做 "Initial Entries" 的测试用例 */
-    describe("Initial Entries",function(){
-      let Entries0=document.getElementsByClassName("entry");
-      beforeEach(function(done) {
-        loadFeed(0,done);
-      });
-        /*
+  /* 写一个叫做 "Initial Entries" 的测试用例 */
+  describe("Initial Entries", function() {
+    let Entries0 = document.getElementsByClassName("entry");
+    beforeEach(function(done) {
+      loadFeed(0, done);
+    });
+    /*
          * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素里面至少有一个 .entry 的元素。
          *
          * 记住loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach、和异步的 done() 函数。
          */
-         it("loadFeed works",function(){
-           console.log(Entries0[0].innerText);
-           expect(Entries0[0]).toBeDefined();
-         });
+    it("loadFeed works", function() {
+      expect(Entries0[0]).toBeDefined();
     });
+  });
 
-    /* 写一个叫做 "New Feed Selection" 的测试用例 */
-    describe("New Feed Selection",function(){
-      let EnIn0,EnIn1;
-      beforeEach(function(done) {
-        let Entries0=document.getElementsByClassName("entry");
-        EnIn0=Entries0[0].innerText;
-        console.log(EnIn0);
-        loadFeed(1,done);
-      });
-       /*
+  /* 写一个叫做 "New Feed Selection" 的测试用例 */
+  describe("New Feed Selection", function() {
+    let EnIn0,
+      EnIn1;
+    beforeEach(function(done) {
+      let Entries0 = document.getElementsByClassName("entry");
+      EnIn0 = Entries0[0].innerText;
+      console.log(EnIn0);
+      loadFeed(1, done);
+    });
+    /*
         * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
         * 记住loadFeed() 函数是异步的。
         */
-       it("loadFeed will change",function(){
-         let Entries1=document.getElementsByClassName("entry");
-         EnIn1=Entries1[0].innerText;
-         console.log(EnIn1);
-         expect(EnIn1).not.toBe(EnIn0);
-       });
+    it("loadFeed will change", function() {
+      let Entries1 = document.getElementsByClassName("entry");
+      EnIn1 = Entries1[0].innerText;
+      console.log(EnIn1);
+      expect(EnIn1).not.toBe(EnIn0);
     });
+  });
 }());
